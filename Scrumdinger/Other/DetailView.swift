c49bc18cf520a26cd9f2c954e58e7998dab9b603
GDDanil <sleepydan36@gmail.com>
2023-03-17 14:45:29 +0300

    init commit

---
+++ Scrumdinger/Other/DetailView.swift
@@ -0,0 +1,22 @@
+//
+//  Detail.swift
+//  Scrumdinger
+//
+//  Created by Danila on 17.03.2023.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
